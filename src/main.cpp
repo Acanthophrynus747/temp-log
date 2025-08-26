65d3b80903529180d6aba74ca6ee6b9836ef3796
Acanthophrynus747 <jacksbuilds@gmail.com>
2025-08-26 01:41:57 -0700

    messing around

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <Wire.h>
 #include <SD.h>
 #include <WiFi.h>
-#include "WifiPass.h"
+#include "WifiPass.h" //little library with my home wifi password
 
 const int DHT_pin = 4;
 const int SD_SDI_pin = 5;
@@ -14,10 +14,21 @@ const int SD_SDI_pin = 5;
 
 File myFile;
 
-// #define DHT_type DHT22;
-
 float hum;
 float temp;
+
+//WiFi stuff https://randomnerdtutorials.com/esp32-web-server-arduino-ide/
+WiFiServer server(80);
+String header;
+String output = "standby";
+unsigned long current_time = millis();
+unsigned long previous_time = 0;
+const long timeout_time = 2000;
+
+bool button_test = false;
+
+void WiFiUpload(void);
+//^^^^^wifi stuff
 
 DHT dht(DHT_pin, DHT22);
 
@@ -29,10 +40,9 @@ RTC_DS3231 rtc; //type of rtc used
 // }
 
 void setup(){
+    Serial.begin(115200);
 
-    //Find the default SPI pins for your board
-    Serial.begin(9600);
-
+    //Find the default SPI pins for the board:
     Serial.print("MOSI: ");
     Serial.println(MOSI);
     Serial.print("MISO: ");
@@ -41,9 +51,26 @@ void setup(){
     Serial.println(SCK);
     Serial.print("SS: ");
     Serial.println(SS);  
-    Serial.println(ssid);
 
     pinMode(DHT_pin, INPUT);
+
+    Serial.println("connecting to wifi");
+    WiFi.begin(ssid, password);
+
+    Serial.print("SSID: ");
+    Serial.println(ssid);
+
+    while (WiFi.status() != WL_CONNECTED){
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("IP Address: ");
+    Serial.println(WiFi.localIP());
+    server.begin();
+    
+    Serial.print("ESP IP: ");
+    Serial.println(WiFi.localIP()); //ESP32 IP
 
     rtc.begin();
     dht.begin();
@@ -62,33 +89,36 @@ void setup(){
         Serial.println("RTC running with correct time");
     }
 
-    if (!SD.begin(SD_SDI_pin)){
-        while (1){
-            Serial.println(F("SD CARD FAILED, OR NOT PRESENT!"));
-            delay(1000);
-        }
-    }
+    // if (!SD.begin(SD_SDI_pin)){
+    //     while (1){
+    //         Serial.println(F("SD CARD FAILED, OR NOT PRESENT!"));
+    //         delay(1000);
+    //     }
+    // }
     
-    Serial.println(F("SD CARD INITIALIZED."));
+    // Serial.println(F("SD CARD INITIALIZED."));
 
     
-    if (!SD.exists("/esp32.txt")) {
-        Serial.println(F("esp32.txt doesn't exist. Creating esp32.txt file..."));
-        // create a new file by opening a new file and immediately close it
-        myFile = SD.open("/esp32.txt", FILE_WRITE);
-        myFile.close();
-    }
+    // if (!SD.exists("/esp32.txt")) {
+    //     Serial.println(F("esp32.txt doesn't exist. Creating esp32.txt file..."));
+    //     // create a new file by opening a new file and immediately close it
+    //     myFile = SD.open("/esp32.txt", FILE_WRITE);
+    //     myFile.close();
+    // }
 
-    // recheck if file is created or not
-    if (SD.exists("/esp32.txt"))
-        Serial.println(F("esp32.txt exists on SD Card."));
-    else
-        Serial.println(F("esp32.txt doesn't exist on SD Card."));
+    // // recheck if file is created or not
+    // if (SD.exists("/esp32.txt"))
+    //     Serial.println(F("esp32.txt exists on SD Card."));
+    // else
+    //     Serial.println(F("esp32.txt doesn't exist on SD Card."));
 
     
 }
 
 void loop(){
+
+    WiFiUpload();
+
     DateTime now = rtc.now();
 
     Serial.print(now.year(), DEC);
@@ -118,30 +148,136 @@ void loop(){
     Serial.print("Temp: ");
     Serial.println(temp);
 
-    myFile = SD.open("/esp32.txt", FILE_WRITE);
+    // myFile = SD.open("/esp32.txt", FILE_WRITE);
 
-    if (myFile) { //MOVE TO ON THE MINUTE EVENTUALLY
-        myFile.print("time: "); //write in the time
-        myFile.print(now.year(), DEC);
-        myFile.print('/');
-        myFile.print(now.month(), DEC);
-        myFile.print('/');
-        myFile.print(now.day(), DEC);
-        myFile.print(" ");
-        myFile.print(now.hour(), DEC);
-        myFile.print(':');
-        myFile.print(now.minute(), DEC);
-        myFile.print(':');
-        myFile.print(now.second(), DEC);
+    // if (myFile) { //MOVE TO ON THE MINUTE EVENTUALLY
+    //     myFile.print("time: "); //write in the time
+    //     myFile.print(now.year(), DEC);
+    //     myFile.print('/');
+    //     myFile.print(now.month(), DEC);
+    //     myFile.print('/');
+    //     myFile.print(now.day(), DEC);
+    //     myFile.print(" ");
+    //     myFile.print(now.hour(), DEC);
+    //     myFile.print(':');
+    //     myFile.print(now.minute(), DEC);
+    //     myFile.print(':');
+    //     myFile.print(now.second(), DEC);
 
-        myFile.print(", humidity: "); // write a line to esp32.txt
-        myFile.print(hum); // write another line to esp32.txt
-        myFile.print(", temp(F): ");
-        myFile.println(temp); //newline after this
-        myFile.close();
-    } else {
-        Serial.print(F("SD Card: Issue encountered while attempting to open the file esp32.txt"));
-    }
+    //     myFile.print(", humidity: "); // write a line to esp32.txt
+    //     myFile.print(hum); // write another line to esp32.txt
+    //     myFile.print(", temp(F): ");
+    //     myFile.println(temp); //newline after this
+    //     myFile.close();
+    // } else {
+    //     Serial.print(F("SD Card: Issue encountered while attempting to open the file esp32.txt"));
+    // }
 
     delay(1000);
+}
+
+void WiFiUpload(){
+    WiFiClient client = server.available(); // Listen for incoming clients
+    if (client) {                             // If a new client connects,
+    current_time = millis();
+    previous_time = current_time;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && current_time - previous_time <= timeout_time) {  // loop while the client's connected
+      current_time = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET buttontest") >= 0) {
+              button_test = true;
+            }
+            // } else if (header.indexOf("GET /26/off") >= 0) {
+            //   Serial.println("GPIO 26 off");
+            //   output26State = "off";
+            //   digitalWrite(output26, LOW);
+            // } else if (header.indexOf("GET /27/on") >= 0) {
+            //   Serial.println("GPIO 27 on");
+            //   output27State = "on";
+            //   digitalWrite(output27, HIGH);
+            // } else if (header.indexOf("GET /27/off") >= 0) {
+            //   Serial.println("GPIO 27 off");
+            //   output27State = "off";
+            //   digitalWrite(output27, LOW);
+            // }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            // client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            // client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            // client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            // client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>Temperature and Humidity</h1>");
+
+            client.print("<p>Temperature: ");
+            client.print(temp);
+            client.print(" F</p>");
+            client.print("<p>Humidity: ");
+            client.print(hum);
+            client.println(" %</p>");
+
+            client.print("<p><a href=\"buttontest\"><button>TEST</button></a></p>");
+
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            // client.println("<p>GPIO 26 - State " + output26State + "</p>");
+            // // If the output26State is off, it displays the ON button       
+            // if (output26State=="off") {
+            //   client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            // } else {
+            //   client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+            // } 
+               
+            // // Display current state, and ON/OFF buttons for GPIO 27  
+            // client.println("<p>GPIO 27 - State " + output27State + "</p>");
+            // // If the output27State is off, it displays the ON button       
+            // if (output27State=="off") {
+            //   client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+            // } else {
+            //   client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+            // }
+            // client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
